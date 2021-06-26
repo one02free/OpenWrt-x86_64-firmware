5c72cd08f44b08ec4d3f2efd7ee5fce63e46565a
one02free <one02free@qq.com>
2021-06-26 21:29:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,11 +13,9 @@
 # Uncomment a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/Mattraks/helloworld.git' feeds.conf.default
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 
-# git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+sed -i '$a src-git helloworld https://github.com/Mattraks/helloworld.git' feeds.conf.default
 rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
 git clone https://github.com/project-lede/luci-app-godproxy.git package/lean/luci-app-godproxy
-git clone https://github.com/linkease/ddnsto-openwrt.git package/network/services/ddnsto-openwrt
-#git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git package/lean/luci-app-unblockneteasemusic
