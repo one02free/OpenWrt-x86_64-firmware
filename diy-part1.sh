ec2d42b8f47ff28396367e50b9cc6937f1d611da
one02free <3870416+one02free@users.noreply.github.com>
2024-10-19 14:30:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,7 +20,8 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.con
 
 #git clone -b istoreos-22.03 https://github.com/istoreos/istoreos.git istoreos
 #rm -rf package/libs/openssl && mv istoreos/package/libs/openssl package/libs/openssl
-rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+rm -rf package/lean/luci-theme-argon && git git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 git clone https://github.com/yaof2/luci-app-ikoolproxy.git package/lean/uci-app-ikoolproxy
 #git clone -b master https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/lean/luci-app-unblockneteasemusic
